d8f2f76fe956e3e2767d19123b220d671d25d353
Joshthedruid <33952609+Joshthedruid@users.noreply.github.com>
2023-08-03 18:46:16 -0500

    "Getting started, part 1 complete"

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Kickass Bouks";
+   let book1 = "https://bookshopapocalypse.com/cdn/shop/products/faultinourstarssigned-1sig_1024x1024@2x.jpg?v=1638668974";
+   let book2 = "https://bookshopapocalypse.com/cdn/shop/files/goosebumpshauntedmask2015reprint-1sq_300x300.jpg?v=1685592486";
+   let book3 = "https://bookshopapocalypse.com/cdn/shop/files/thewalking-1sq_1024x1024@2x.jpg?v=1689633848";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Sorry too big" />
+         <img src={book2} alt="Also too big" />
+         <img src={book3} alt="Aaaaaaa" />
       </div>      
    );
 }
